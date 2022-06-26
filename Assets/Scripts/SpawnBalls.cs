84abc29d6b2ada501846ef6ad50ceb42c28413f9
Kotob M <realkotob@gmail.com>
2022-06-27 00:45:06 +0300

    Add ball prefab, materials, and spawn several at start

---
+++ Assets/Scripts/SpawnBalls.cs
@@ -0,0 +1,67 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnBalls : MonoBehaviour
+{
+    public Transform ballSpawnPoint;
+    public GameObject ballPrefab;
+    public List<Material> ballMaterials;
+
+    
+    int ballMaterialIndex = 0;
+
+    void Start()
+    {
+        Shuffle(ballMaterials);
+
+        for (int i = 0; i < 20; i++)
+        {
+            SpawnBall();
+        }
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            SpawnBall();
+        }
+    }
+
+    void SpawnBall()
+    {
+        float xOffset = Random.Range(-0.7f, 0.7f);
+        float yOffset = Random.Range(-0.7f, 0.7f);
+        float zOffset = Random.Range(-0.7f, 0.7f);
+        Vector3 spawnPoint = new Vector3(ballSpawnPoint.position.x + xOffset, ballSpawnPoint.position.y + yOffset,
+                                         ballSpawnPoint.position.z + zOffset);
+
+        GameObject ball = Instantiate(ballPrefab, spawnPoint, ballSpawnPoint.rotation);
+        MeshRenderer ballMeshRenderer = ball.GetComponentInChildren<MeshRenderer>();
+        ballMeshRenderer.material = pickRandomMaterial();
+    }
+
+    Material pickRandomMaterial()
+    {
+        var material = ballMaterials[ballMaterialIndex];
+        ballMaterialIndex++;
+        if (ballMaterialIndex >= ballMaterials.Count)
+        {
+            ballMaterialIndex = 0;
+            Shuffle(ballMaterials);
+        }
+        return material;
+    }
+
+    void Shuffle<T>(List<T> list)
+    {
+        for (int i = 0; i < list.Count; i++)
+        {
+            int j = Random.Range(i, list.Count);
+            T temp = list[i];
+            list[i] = list[j];
+            list[j] = temp;
+        }
+    }
+}
